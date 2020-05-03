8172e3faaad9a9782ab6067fb17e11c30bf8f3fc
Will Paul <wpaul@constantcontact.com>
2020-05-03 01:37:00 -0400

    Deploy 72ac409-dirty as v72ac4091729cf2187b33c3585041916c223e47c6

--- build.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-rm -rf target/ && \
-  ./gen_index.sh && \
-  ./gen_html.sh && \
-  cp styles.css target/styles.css
--- publish.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-git checkout gh-pages
-git add -f target/
-git commit -m "Deploying $(git rev-parse HEAD)"
-git push -f origin `git subtree split --prefix target`:gh-pages
-git checkout master

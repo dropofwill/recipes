2e6b690396b59ab03c9eb87e1748c5b6c36cbc8f
Will Paul <wpaul@constantcontact.com>
2020-05-05 00:50:47 -0400

    First pass at generating a tag listing
    * Use rg to find all the tags in recipes, basically #blah
    * sort | uniq -c | sort to get them all with counts
    * Build a list of links from that
    
    Now just need to make the tag page for each link..

--- gen_index.sh
+++ gen_index.sh
@@ -1,30 +1,55 @@
-#! /bin/bash
+#! /bin/zsh
+set -e
+
+mkdir -p target/
 
 # Generate TOC as index.html for all the recipes in this repo
+cat \
+  <(echo "> No story. Just the recipe. Use as is or upload the .recipe to [Grocery](https://apps.apple.com/us/app/grocery-smart-shopping-list/id1195676848)") \
+  <(echo "<div>") \
+  <(echo '') \
+  <(echo '#### Appetizers') \
+  <(git ls-files appetizers/*.recipe | \
+    xargs -I {} bash -c 'echo - [$(basename {} .recipe)]\($(dirname {})/$(basename {} .recipe).html\)' ) \
+  <(echo '') \
+  <(echo '#### Breads') \
+  <(git ls-files breads/*.recipe | \
+    xargs -I {} bash -c 'echo - [$(basename {} .recipe)]\($(dirname {})/$(basename {} .recipe).html\)' ) \
+  <(echo '') \
+  <(echo '#### Coffees') \
+  <(git ls-files coffees/*.recipe | \
+    xargs -I {} bash -c 'echo - [$(basename {} .recipe)]\($(dirname {})/$(basename {} .recipe).html\)' ) \
+  <(echo '') \
+  <(echo '#### Desserts') \
+  <(git ls-files desserts/*.recipe | \
+    xargs -I {} bash -c 'echo - [$(basename {} .recipe)]\($(dirname {})/$(basename {} .recipe).html\)' ) \
+  <(echo '') \
+  <(echo '#### Meals') \
+  <(git ls-files meals/*.recipe | \
+    xargs -I {} bash -c 'echo - [$(basename {} .recipe)]\($(dirname {})/$(basename {} .recipe).html\)' ) \
+  <(echo "</div>") \
+  <(echo '') \
+  <(echo "# Tags") \
+  <(echo '') \
+  <(rg --no-line-number --no-heading --no-filename -o '\#[A-Za-z]+\b' \
+    {appetizers,breads,coffees,desserts,meals}/*.recipe | \
+      sort | uniq -c | sort -r | \
+      xargs -I {} bash -c 'echo "- [{}](tags/$(echo "{}" | sed s/.*\#// ).html)"') \
+    | \
+    pandoc -s -f markdown -t html5 --css styles.css --metadata title="Recipe list" \
+    -o target/index.html
 
-mkdir -p target/ &&
-  cat \
-    <(echo "> No story. Just the recipe. Use as is or upload the .recipe to [Grocery](https://apps.apple.com/us/app/grocery-smart-shopping-list/id1195676848)") \
-    <(echo '') \
-    <(echo '## Appetizers') \
-    <(git ls-files appetizers/*.recipe | \
-      xargs -I {} bash -c 'echo - [$(basename {} .recipe)]\($(dirname {})/$(basename {} .recipe).html\)' ) \
-    <(echo '') \
-    <(echo '## Breads') \
-    <(git ls-files breads/*.recipe | \
-      xargs -I {} bash -c 'echo - [$(basename {} .recipe)]\($(dirname {})/$(basename {} .recipe).html\)' ) \
-    <(echo '') \
-    <(echo '## Coffees') \
-    <(git ls-files coffees/*.recipe | \
-      xargs -I {} bash -c 'echo - [$(basename {} .recipe)]\($(dirname {})/$(basename {} .recipe).html\)' ) \
-    <(echo '') \
-    <(echo '## Desserts') \
-    <(git ls-files desserts/*.recipe | \
-      xargs -I {} bash -c 'echo - [$(basename {} .recipe)]\($(dirname {})/$(basename {} .recipe).html\)' ) \
-    <(echo '') \
-    <(echo '## Meals') \
-    <(git ls-files meals/*.recipe | \
-      xargs -I {} bash -c 'echo - [$(basename {} .recipe)]\($(dirname {})/$(basename {} .recipe).html\)' ) \
-      | pandoc -s -f markdown -t html5 --css styles.css --metadata title="Recipe list" -o target/index.html
-
+# Find all the tags in the recipe
+# Sum them up by uniq counts
+# Generate markdown list with links to their own pages
+cat \
+  <(echo "> Recipes by tag") \
+  <(echo '') \
+  <(rg --no-line-number --no-heading --no-filename -o '\#[A-Za-z]+\b' \
+    {appetizers,breads,coffees,desserts,meals}/*.recipe | \
+      sort | uniq -c | sort -r | \
+      xargs -I {} bash -c 'echo "- [{}](tags/$(echo "{}" | sed s/.*\#// ).html)"') \
+      | \
+      pandoc -s -f markdown -t html5 --css styles.css --metadata title="Tags" \
+      -o target/tags.html
 
